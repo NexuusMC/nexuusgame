cc09af7fe5ac11ad8132ad3ff97734f562f55317
NexuusMC <nexusmc69@gmail.com>
2015-06-05 12:52:43 +0100

    First Commit - Going to school, code in progress:)

---
+++ src/nexuusgame/main/nexuusgamemain.java
@@ -0,0 +1,16 @@
+package nexuusgame.main;
+
+public class nexuusgamemain {
+	
+	//Main file - Copyright - Christopher Albon;
+	//Started the project on 5th June 2015, 12:40
+		
+	//Basically a game, and coded when leaving year 11 :)
+		
+	public static void Main(String[] args) {
+		
+		
+		
+	}
+
+}
